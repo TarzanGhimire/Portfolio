c64cb1634e0b40f8b9cb444dc1fd059094cb32c7
Tarzan <tarzan.077bct092@acem.edu.np>
2025-01-02 21:13:49 +0545

    first commit

---
+++ src/data/seo.js
@@ -0,0 +1,38 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"I am a Computer Engineering Student with expertise in Node.js. I am also familiar to Python and Django",
+		keywords: ["Tarzan", "Tarzan G", "Tarzan Ghimire"],
+	},
+
+	{
+		page: "about",
+		description:
+			"I am a Computer Engineering Student with expertise in Node.js. I am also familiar to Python and Django. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
+		keywords: ["Tarzan", "Tarzan G", "Tarzan Ghimire"],
+	},
+
+	{
+		page: "articles",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+		keywords: ["Tarzan", "Tarzan G", "Tarzan Ghimire"],
+	},
+
+	{
+		page: "projects",
+		description:
+			"I've worked on a variety of projects over the years. Many of these projects are open-source and available for others to explore and contribute to.",
+		keywords: ["Tarzan", "Tarzan G", "Tarzan Ghimire"],
+	},
+
+	{
+		page: "contact",
+		description:
+			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
+		keywords: ["Tarzan", "Tarzan G", "Tarzan Ghimire"],
+	},
+];
+
+export default SEO;
---
+++ src/data/user.js
@@ -0,0 +1,67 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by Tarzan",
+		name: "Tarzan Ghimire",
+		email: "tarzanghimire92@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://x.com/Tarzan_Ghimire_",
+		github: "https://github.com/TarzanGhimire",
+		linkedin: "https://www.linkedin.com/in/tarzanghimire/",
+		instagram: "https://www.instagram.com/tarzanghimire.92/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://www.facebook.com/tarzanghimire.9292/",
+	},
+
+	homepage: {
+		title: "Tarzan Ghimire: Personal Portfolio",
+		description:
+			"I am a Computer Engineering Student with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I’m Tarzan G. I live Lalitpur,Nepal.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Memory Game",
+			description:
+				"A memory testing game made with flipcards for increasiing memory while having fun",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Car Insights System",
+			description:
+				"An AI model trained in yolo to recognize any vehicle company, maker, model number and al its specifications through just a photo. ",
+			logo: "https://editor.analyticsvidhya.com/uploads/82763vehicle-detection-recognition-system-500x500.jpg",
+			linkText: "View Project",
+			link: "https://github.com/TarzanGhimire/CarInsights",
+		},
+
+		{
+			title: "SkillHunt",
+			description:
+				"A platform to host and participate in Hackathons from around the world.",
+			logo: "https://camo.githubusercontent.com/6101e13305a95d43371854b5c506e9c27e7340c830180e60e317ae3ce175f697/68747470733a2f2f626c6f676765722e676f6f676c6575736572636f6e74656e742e636f6d2f696d672f612f415676587345694d567a5a4d4a6a74514e736b69672d674739656357654752706379456a485236574c4133475447787573654d7a4855656736364d7250536639625a5a437753624e684f647477424a7268794b77726f4942694250346b535056624d45474f3937556f776b364a36515a6147313376524e4446466c7446743067434673653538596673705137463062355a495370623073313962655957794964364f534d4f62587572697a77585671786239677a58696136527643446b76414a69414d393d773535322d68323435",
+			linkText: "View Project",
+			link: "https://github.com/SamratNeupane787/skillHunt",
+		},
+
+	],
+};
+
+export default INFO;
